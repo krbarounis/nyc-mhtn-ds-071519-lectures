8b41b22e966787a0e38f0db6b2e92af7fe9fdb52
Kristina Barounis <krbarounis@gmail.com>
2019-07-23 16:13:57 -0400

    sql

---
+++ week-2/SQL/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:28 2019
+
+@author: Kristinabarounis
+"""
+
+host = 'flatiron.cdy7lrwxwrdc.us-east-2.rds.amazonaws.com'
+user = 'krbarounis'
+password = 'Sf2rwa13?'
